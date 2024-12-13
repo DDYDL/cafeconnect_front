9f99e622657af47f161e626dd84e70089a171108
test <ccccssung@naver.com>
2024-12-13 13:45:32 +0900

    commit

--- src/component/MainStore/RepairList.js
+++ src/component/MainStore/RepairList.js
@@ -50,7 +50,7 @@ function RepairListCopy() {
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
 
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return `${year}년-${month}월-${day}일`;
   };
   const handleNavigate = (index) => () => {
     const page = pageList[index];
